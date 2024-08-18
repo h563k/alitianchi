90e2c9e40b7da492440066e6f6933ac0dc7fd4d1
yang <yang@gmail.com>
2024-08-19 00:48:23 +0800

    promot

--- src/rag_sample.py
+++ src/rag_sample.py
@@ -1,18 +1,29 @@
 from openai import OpenAI
+import yaml
+
+
+def promot_read():
+    with open('scripts/prompt_settings.yaml', 'r', encoding='utf-8') as f:
+        data = yaml.load(f, Loader=yaml.FullLoader)
+    return data
 
 
 def get_response(prompt, question):
     client = OpenAI(base_url="http://127.0.0.1:8000/v1",
                     api_key="0")
     response = client.chat.completions.create(
-        model="huatuo",
+        model="baichuan-chat-7b",
         messages=[
             {"role": "system", "content": prompt},
             {"role": "user", "content": question},
         ],
-        temperature=0,
-        max_tokens=100,
+        temperature=0.1,
+        max_tokens=2048,
         top_p=1,
         frequency_penalty=0,
     )
     return response.choices[0].message.content
+
+
+if __name__ == '__main__':
+    print(promot_read())
